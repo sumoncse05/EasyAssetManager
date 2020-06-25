78521c3ae1707692e68115b9885ef55b438e63e3
Hasinul Islam <37095367+Hasinul@users.noreply.github.com>
2020-06-25 13:03:05 +0600

    tbl

---
+++ EasyAssetManager/Controllers/SMEController.cs
@@ -0,0 +1,23 @@
+﻿using EasyAssetManagerCore.BusinessLogic.Operation;
+using EasyAssetManagerCore.BusinessLogic.Security;
+using EasyAssetManagerCore.Models.CommonModel;
+using EasyAssetManagerCore.Models.EntityModel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace EasyAssetManager.Controllers
+{
+    public class SMEController : BaseController
+    {
+        private IHostingEnvironment environment;
+        public SMEController(IHostingEnvironment environment)
+        {
+            this.environment = environment;
+        }
+        public IActionResult BranchWiseRetailReport()
+        {
+            return View();
+        }
+    }
+}
