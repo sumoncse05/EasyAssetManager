19907d4f3d94dec9d1a3461e854662a528938528
Hasinul Islam <37095367+Hasinul@users.noreply.github.com>
2020-07-14 15:35:47 +0600

    exception handelling

--- EasyAssetManagerCore/BusinessLogic/Operation/Asset/RMAssetManager.cs
+++ EasyAssetManagerCore/BusinessLogic/Operation/Asset/RMAssetManager.cs
@@ -20,26 +20,61 @@ namespace EasyAssetManagerCore.BusinessLogic.Operation.Asset
 
         public IEnumerable<Loan> GetUnAuthorizeRM(string loanType, string branchCode, string rmCode,AppSession session)
         {
-            return rmRepository.GetUnAuthorizeLoanRM(loanType, branchCode, rmCode, session.User.user_id);
+            try
+            {
+                return rmRepository.GetUnAuthorizeLoanRM(loanType, branchCode, rmCode, session.User.user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         public IEnumerable<Branch> GetBranchList(string pvc_areacode, string user_id)
         {
-            return rmRepository.GetBranchList(pvc_areacode,user_id);
+            try
+            {
+                return rmRepository.GetBranchList(pvc_areacode,user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         public IEnumerable<Area> GetAreaList(string user_id)
         {
-            return rmRepository.GetAreaList(user_id);
+            try
+            {
+                return rmRepository.GetAreaList(user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public IEnumerable<LoanProduct> GetLoanProductList(string loanType, string user_id)
         {
-            return rmRepository.GetLoanProduct(loanType, user_id);
+            try
+            {
+                return rmRepository.GetLoanProduct(loanType, user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         public IEnumerable<RM> GetRmList(string branch_code,string user_id)
         {
-            return rmRepository.GetRMList(branch_code, user_id);
+            try
+            {
+                return rmRepository.GetRMList(branch_code, user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         public Message SetAssignRM(List<string> loans, string newRmCode, string effectDate, AppSession session)
@@ -116,28 +151,70 @@ namespace EasyAssetManagerCore.BusinessLogic.Operation.Asset
 
         public IEnumerable<Loan> GetAvailableLoan(string loanType, string loanProduct, string rmCode, AppSession session)
         {
-            return rmRepository.GetAvailableLoan(loanType, loanProduct, rmCode,session.User.user_id);
+            try
+            {
+                return rmRepository.GetAvailableLoan(loanType, loanProduct, rmCode, session.User.user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         public IEnumerable<Designation> GetDesignationList(string user_id)
         {
-            return rmRepository.GetDesignationList(user_id);
+            try
+            {
+                return rmRepository.GetDesignationList(user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public IEnumerable<Grade> GetGradeList(string user_id)
         {
-            return rmRepository.GetGradeList(user_id);
+            try
+            {
+                return rmRepository.GetGradeList(user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public IEnumerable<Department> GetDepartmentList(string user_id)
         {
-            return rmRepository.GetDepartmentList(user_id);
+            try
+            {
+                return rmRepository.GetDepartmentList(user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public IEnumerable<Category> GetCategoryList(string user_id)
         {
-            return rmRepository.GetCategoryList(user_id);
+            try
+            {
+                return rmRepository.GetCategoryList(user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public IEnumerable<RMStatus> GetRMStatusList(string user_id)
         {
-            return rmRepository.GetRMStatusList(user_id);
+            try
+            {
+                return rmRepository.GetRMStatusList(user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         
         public Message SetRM(RM rm, AppSession session)
@@ -196,24 +273,45 @@ namespace EasyAssetManagerCore.BusinessLogic.Operation.Asset
         }
         public RM GetRMDetails(string emp_id, string user_id)
         {
-            var rmDetails = rmRepository.GetRMDetails(emp_id,user_id);
-            if (rmDetails == null)
+            try
             {
-                rmDetails = new RM();
-                MessageHelper.Error(Message,"No Employee found");
-            } 
-            else
-                MessageHelper.Success(Message, "Employee found");
-            rmDetails.Message = Message;
-            return rmDetails;
+                var rmDetails = rmRepository.GetRMDetails(emp_id, user_id);
+                if (rmDetails == null)
+                {
+                    rmDetails = new RM();
+                    MessageHelper.Error(Message, "No Employee found");
+                }
+                else
+                    MessageHelper.Success(Message, "Employee found");
+                rmDetails.Message = Message;
+                return rmDetails;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public IEnumerable<RM> GetRMDetailList(string emp_id,string rm_name, string user_id)
         {
-            return rmRepository.GetRMDetailList(emp_id, rm_name, user_id);
+            try
+            {
+                return rmRepository.GetRMDetailList(emp_id, rm_name, user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public IEnumerable<Loan> GetLoanDetailList(string ac_area_code, string ac_branch_code, string ac_loan_no, string user_id)
         {
-            return rmRepository.GetLoanDetailList(ac_area_code, ac_branch_code, ac_loan_no, user_id);
+            try
+            {
+                return rmRepository.GetLoanDetailList(ac_area_code, ac_branch_code, ac_loan_no, user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public Message UpdateMonitoringRM(string loan_ac_no, string moni_rm_code, AppSession session)
         {
@@ -243,11 +341,25 @@ namespace EasyAssetManagerCore.BusinessLogic.Operation.Asset
         }
         public DashBord GetDashBoardDetails(string user_id)
         {
-            return rmRepository.GetDashBoardDetails( user_id);
+            try
+            {
+                return rmRepository.GetDashBoardDetails(user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public IEnumerable<DashBord> GetHalfYearlyDashBoard(string user_id)
         {
-            return rmRepository.GetHalfYearlyDashBoard(user_id);
+            try
+            {
+                return rmRepository.GetHalfYearlyDashBoard(user_id);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public IEnumerable<AstDailyStatus> GetYearlyReportData(string area_code, string branch_code, string rm_code, string loantype, AppSession session)
         {
